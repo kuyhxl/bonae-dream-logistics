adc5c137f3e1d748885cd150748edb58dcd8138c
kuyhxl <ozzing727@naver.com>
2026-08-04 21:15:03 +0900

    fix(common): AuditorAware 클래스 명 상수명 오타 수정

--- common/src/main/java/com/bonae/logistics/common/config/AuditorAwareImpl.java
+++ common/src/main/java/com/bonae/logistics/common/config/AuditorAwareImpl.java
@@ -2,24 +2,26 @@ package com.bonae.logistics.common.config;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.lang.NonNull;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.Optional;
 
-public class AuditorAwareImple implements AuditorAware<String> {
+public class AuditorAwareImpl implements AuditorAware<String> {
 
     public static final String SYSTEM = "SYSTEM";
-    private static final String USER_ID_HEADR = "X-User_Id";
+    private static final String USER_ID_HEADER = "X-User-Id";
 
     @Override
+    @NonNull
     public Optional<String> getCurrentAuditor() {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
 
         if(attributes == null) return Optional.of(SYSTEM);
 
         HttpServletRequest request = attributes.getRequest();
-        String userId = request.getHeader(USER_ID_HEADR);
+        String userId = request.getHeader(USER_ID_HEADER);
 
         return Optional.of(
                 userId == null || userId.isBlank() ? SYSTEM : userId
