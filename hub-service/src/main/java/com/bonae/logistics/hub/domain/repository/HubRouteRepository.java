7da105a6fb7b1a19252163cb55ff0ebebccc5d80
ssongcookie <89139633+ssongcookie@users.noreply.github.com>
2026-08-05 19:06:21 +0900

    fix(hub): HubRouteRepository 제네릭 타입 Hub -> HubRoute 수정

--- hub-service/src/main/java/com/bonae/logistics/hub/domain/repository/HubRouteRepository.java
+++ hub-service/src/main/java/com/bonae/logistics/hub/domain/repository/HubRouteRepository.java
@@ -1,9 +1,9 @@
 package com.bonae.logistics.hub.domain.repository;
 
-import com.bonae.logistics.hub.domain.entity.Hub;
+import com.bonae.logistics.hub.domain.entity.HubRoute;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface HubRouteRepository extends JpaRepository<Hub, UUID> {
+public interface HubRouteRepository extends JpaRepository<HubRoute, UUID> {
 }
