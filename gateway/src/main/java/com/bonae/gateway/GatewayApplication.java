9a26c434ba2aeb25a3b69fb38dbc422a961de268
kuyhxl <ozzing727@naver.com>
2026-08-08 17:44:50 +0900

    feat(gateway): JWT 인증 필터 및 사용자 헤더 주입 구현

--- gateway/src/main/java/com/bonae/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/bonae/gateway/GatewayApplication.java
@@ -1,12 +1,13 @@
 package com.bonae.gateway;
 
+import com.bonae.gateway.config.AuthPathProperties;
 import com.bonae.gateway.config.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(JwtProperties.class)
+@EnableConfigurationProperties({JwtProperties.class, AuthPathProperties.class})
 public class GatewayApplication {
 
 	public static void main(String[] args) {
