b35a001a661adf1c52ce86e46f3177dc308f0ee6
coco <meunse1999@gmail.com>
2026-08-10 23:11:32 +0900

    chore: application.yaml 수정

--- order-service/src/main/java/com/bonae/logistics/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/bonae/logistics/order/OrderServiceApplication.java
@@ -2,11 +2,13 @@ package com.bonae.logistics.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.retry.annotation.EnableRetry;
 
 @EnableRetry
 @EnableFeignClients
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class OrderServiceApplication {
 
