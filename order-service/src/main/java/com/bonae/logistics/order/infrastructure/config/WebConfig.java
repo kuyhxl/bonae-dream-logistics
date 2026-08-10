e08fc10c4e6944013445780a613fa109bf4e0749
coco <meunse1999@gmail.com>
2026-08-10 11:25:38 +0900

    feat: WebConfig추가 인터셉터 등록

---
+++ order-service/src/main/java/com/bonae/logistics/order/infrastructure/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.bonae.logistics.order.infrastructure.config;
+
+import com.bonae.logistics.order.infrastructure.web.AuthorizationInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthorizationInterceptor authorizationInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authorizationInterceptor)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/internal/**");
+    }
+}
