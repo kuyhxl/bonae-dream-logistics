e100b6424e14db80ae6850021aa575aedf38dee5
coco <meunse1999@gmail.com>
2026-08-11 15:44:28 +0900

    feat: 주문 취소 api 구현

--- order-service/src/main/java/com/bonae/logistics/order/domain/repository/OrderRepository.java
+++ order-service/src/main/java/com/bonae/logistics/order/domain/repository/OrderRepository.java
@@ -3,7 +3,10 @@ package com.bonae.logistics.order.domain.repository;
 import com.bonae.logistics.order.domain.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
+
+    Optional<Order> findByIdAndDeletedAtIsNull(UUID id);
 }
