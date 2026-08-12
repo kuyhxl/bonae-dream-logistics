5479bd26d614ff757ea44959d2bd39ee2b850216
buddle031 <buddle031@gmail.com>
2026-08-12 10:59:29 +0900

    refactor(delivery): 내부 배송 수정 경로 update로 정리

--- delivery-service/src/main/java/com/bonae/logistics/delivery/presentation/controller/InternalDeliveryController.java
+++ delivery-service/src/main/java/com/bonae/logistics/delivery/presentation/controller/InternalDeliveryController.java
@@ -32,7 +32,7 @@ public class InternalDeliveryController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    @PatchMapping("/order")
+    @PatchMapping("/update")
     public ResponseEntity<DeliveryDetailResponse> updateDeliveryByOrder(
             @Valid @RequestBody InternalDeliveryUpdateRequest request
     ) {
