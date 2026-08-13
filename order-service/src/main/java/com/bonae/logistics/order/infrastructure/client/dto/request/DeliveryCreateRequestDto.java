c18001040dd2fb198877cb4314bfc98844d86c82
coco <meunse1999@gmail.com>
2026-08-13 23:06:52 +0900

    feat: dto에 필드 추가

--- order-service/src/main/java/com/bonae/logistics/order/infrastructure/client/dto/request/DeliveryCreateRequestDto.java
+++ order-service/src/main/java/com/bonae/logistics/order/infrastructure/client/dto/request/DeliveryCreateRequestDto.java
@@ -1,5 +1,6 @@
 package com.bonae.logistics.order.infrastructure.client.dto.request;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 //배송 생성 요청 Dto
@@ -8,6 +9,8 @@ public record DeliveryCreateRequestDto(
         UUID supplierCompanyId,
         UUID receiverCompanyId,
         String receiverUsername,
-        String productInfo,
+        String productName,
+        Integer quantity,
+        LocalDateTime dueDate,
         String requestNote  // Order의 remarks와 매핑됨
 ) {}
